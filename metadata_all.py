a7d3e4ebcce1f39019fc3c5abd954f7449eb4855
Harry <ha@hg.com>
2022-04-20 20:50:28 +0300

    * add fields in csv

--- metadata_all.py
+++ metadata_all.py
@@ -20,7 +20,12 @@ EXPLICIT_AND_SENSITIVE_CONTENT = conf["EXPLICIT_AND_SENSITIVE_CONTENT"]
 SUPPLY = conf["SUPPLY"]
 BLOCKCHAIN = conf["BLOCKCHAIN"]
 EXTERNAL_LINK = conf["EXTERNAL_LINK"]
-
+SALE_TYPE = conf["SALE_TYPE"]
+PRICE = conf["PRICE"]
+METHOD = conf["METHOD"]
+DURATION = conf["DURATION"]
+SPECIFIC_BUYER = conf["SPECIFIC_BUYER"]
+QUANTITY = conf["QUANTITY"]
 
 def generate_metadata_all(all_images):
     token_path = prepare_file_info("./images")
@@ -66,45 +71,41 @@ def generate_metadata_all(all_images):
         csv_row += ";;" # for double delimiter
         csv_row += BLOCKCHAIN  # blockchain
         csv_row += ";;" # for double delimiter
+        csv_row += SALE_TYPE
+        csv_row += ";;"  # for double delimiter
+        csv_row += str(PRICE)
+        csv_row += ";;"  # for double delimiter
+        csv_row += json.dumps(METHOD)
+        csv_row += ";;"  # for double delimiter
+        csv_row += json.dumps(DURATION)
+        csv_row += ";;"  # for double delimiter
+        csv_row += json.dumps(SPECIFIC_BUYER)
+        csv_row += ";;"  # for double delimiter
+        csv_row += str(QUANTITY)
         csv_data.append(csv_row)
 
-
-        # csv_row.append(token_path[tokenId]) # file_path
-        # csv_row.append("") # for double delimiter
-        # csv_row.append(f"{PROJECT_NAME} #{tokenId}") #nft_name
-        # csv_row.append("")  # for double delimiter
-        # csv_row.append(EXTERNAL_LINK) #nft_name
-        # csv_row.append("")  # for double delimiter
-        # csv_row.append(PROJECT_DESC)  # description
-        # csv_row.append("")  # for double delimiter
-        # csv_row.append(COLLECTION)  # collection
-        # csv_row.append("")  # for double delimiter
-        # csv_row.append(image_properties)  # properties
-        # csv_row.append("")  # for double delimiter
-        # csv_row.append(LEVELS)  # levels
-        # csv_row.append("")  # for double delimiter
-        # csv_row.append(STATS)  # stats
-        # csv_row.append("")  # for double delimiter
-        # csv_row.append(UNLOCKABLE_CONTENT)  # unlockable_content
-        # csv_row.append("")  # for double delimiter
-        # csv_row.append(EXPLICIT_AND_SENSITIVE_CONTENT)  # explicit_and_sensitive_content
-        # csv_row.append("")  # for double delimiter
-        # csv_row.append(SUPPLY)  # supply
-        # csv_row.append("")  # for double delimiter
-        # csv_row.append(BLOCKCHAIN)  # blockchain
-        # csv_row.append("")  # for double delimiter
-        # csv_data.append(csv_row)
-        # csv_row.append("")  # for double delimiter
     with open(METADATA_FILE_NAME, "w") as outfile:
         json.dump(all_images, outfile, indent=4)
 
-    # csv_header = ["file_path", "", "nft_name", "","external_link", "","description", "","collection", "","properties", "","levels", "","stats", "","unlockable_content", "","explicit_and_sensitive_content", "","supply", "","blockchain"]
-    # with open(METADATA_CSV_FILE_NAME, 'w', newline = '') as csvfile:
-    #     # my_writer = csv.writer(csvfile, delimiter = ';')
-    #     my_writer = csv.writer(csvfile, delimiter=';',quoting=csv.QUOTE_MINIMAL)
-    #     my_writer.writerow(csv_header)
-    #     my_writer.writerows(csv_data)
-    csv_header = "file_path;;nft_name;;external_link;;description;;collection;;properties;;levels;;stats;;unlockable_content;;explicit_and_sensitive_content;;supply;;blockchain"
+    csv_header = "file_path;;"
+    csv_header += "nft_name;;"
+    csv_header += "external_link;;"
+    csv_header += "description;;"
+    csv_header += "collection;;"
+    csv_header += "properties;;"
+    csv_header += "levels;;"
+    csv_header += "stats;;"
+    csv_header += "unlockable_content;;"
+    csv_header += "explicit_and_sensitive_content;;"
+    csv_header += "supply;;"
+    csv_header += "blockchain;;"
+    csv_header += "sale_type;;"
+    csv_header += "price;;"
+    csv_header += "method;;"
+    csv_header += "duration;;"
+    csv_header += "specific_buyer;;"
+    csv_header += "quantity"
+
     with open(METADATA_CSV_FILE_NAME, 'w', newline = '') as csvfile:
         csvfile.write(csv_header)
         csvfile.write("\n")
